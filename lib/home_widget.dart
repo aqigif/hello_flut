ab42ac550a4afa7b0cb769fe5871fbe9023a9065
aqil gifari <aqilgifari0@gmail.com>
2019-10-07 11:35:01 +0700

    adding searchbar and promo

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -3,12 +3,44 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // return Scaffold()
     return Scaffold(
-      body: 
-        Center(
-          child: Text("Hello"),
-      )
-    );
+        body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+      Row(children: [
+        Expanded(
+            child: Container(
+          margin: const EdgeInsets.only(top: 40.0, left: 20.0,  right: 20.0),
+          height: 50.0,
+          padding: const EdgeInsets.all(10),
+          decoration: new BoxDecoration(
+            borderRadius: new BorderRadius.circular(25.0),
+            color: Colors.blueGrey[50],
+          ),
+          child: Row(
+            children: [
+              Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  'Search your comfort room',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )),
+        Container(
+          margin: const EdgeInsets.only(top: 40.0, right: 20.0),
+            child: Icon(
+          Icons.confirmation_number,
+          color: Colors.yellow[700],
+        ))
+      ]),
+    ]));
   }
 }
